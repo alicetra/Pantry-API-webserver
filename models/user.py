b313257b8d472a3647ac75dbb4baddef823d8af2
alicetra <alicetra0109@gmail.com>
2023-12-06 20:23:54 +1100

    added more comments to user but also commented setup

--- models/user.py
+++ models/user.py
@@ -38,6 +38,14 @@ class User(db.Model):
     def check_security_answer(self, security_answer):
         return self.check_hash(self.security_answer, security_answer)
 
+    # Benefits of using static methods for validation:
+    # 1. Flexibility: Can be called without creating an instance of the class.
+    # 2. Consistency: Ensures the same validation rules are applied everywhere.
+    # 3. Reusability: Can be called from anywhere in the code.
+    # 4. JSON Compatibility: Works well with JSON data, allowing validation before object creation.
+
+    #This staticmethod validates the password based on several conditions such as length, 
+    #presence of uppercase and lowercase letters, numbers, and special characters.
     @staticmethod
     def validate_password(password):
         if len(password) < 8:
@@ -50,16 +58,19 @@ class User(db.Model):
             raise ValidationError("Password must contain at least one number.")
         elif not re.search(r'\W', password):
             raise ValidationError("Password must contain at least one special character.")
-        #This line prevent a password with spaces in the at any point in of the string to be accepted as it would still pass the special character check without this line.
+        #This line prevent a password with spaces at any point in of the string to be accepted as it would still pass the special character check without this line.
         #Decided to keep this as a seperate requirement instead of changing the special character code to make the requirement of a password explicit.
         elif ' ' in password:
             raise ValidationError("Password must not contain spaces.")
 
+    #This staticmethod validates the username. It checks if the username only contains alphanumeric characters.
     @staticmethod
     def validate_username(username):
         if not re.match('^[a-zA-Z0-9]*$', username):
             raise ValidationError("Username can only contain alphanumeric characters. Special character and spaces are not accepted.")
 
+    #This staticmethod validates the email based on several conditions such as length and format.
+    #It also normalizes the email address by converting the domain part to lowercase.
     @staticmethod
     def validate_email(email):
         if len(email) > 320:
@@ -72,18 +83,20 @@ class User(db.Model):
         email = '@'.join([email_username, email_domain.lower()])
         
         return email
-        
+
+    #This staticmethod validates the security answer. It checks if the security answer only contains alphabetic characters.
     @staticmethod
     def validate_security_answer(security_answer):
         if not security_answer.isalpha():
             raise ValidationError("Security answer must only contain alphabetic characters. Special character and spaces are not accepted. Please note that answer will be case-insentitive")
 
 #This event listener decorator means that the function that follows will be executed after a new User record is inserted into the database. 
-#Since I want all users to have one pantry it made sense to automatically create one as a user is created their database is too.
+#Since I want all users to have one pantry it made sense to automatically create one. 
 @event.listens_for(User, 'after_insert')
 #connection is the active database connection, and target is the actual User table that was just inserted. 
 #SQLAlchemy will still pass three arguments when the after_insert event is triggered hence why by convention I used _ as a placeholder for a parameter that is not used/needed.
 def create_pantry(_, connection, target):
     #This line creates a new Pantry object associated with the newly created User. The name is set to a string that includes the username of the User
     pantry = Pantry(user_id=target.id, name=f"{target.username}'s Pantry")
+    #A commit is not needed here because Flask-SQLAlchemy automatically commits the session at the end of the request. Adding a commit result in a ResourceClosedError 
     db.session.add(pantry)
--- setup.py
+++ setup.py
@@ -6,15 +6,28 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 
+# Load environment variables
 load_dotenv()
 
+# Get the database password from environment variables
 password = os.getenv('DB_PASSWORD')
 
+# Initialize Flask app
 app = Flask(__name__)
+
+# Configure SQLAlchemy with the database URI. This uses the psycopg2 adapter for PostgreSQL.
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://flask_db_dev:{os.getenv("DB_PASSWORD")}@localhost/flask_db'
+
+# Initialize SQLAlchemy with the Flask app
 db = SQLAlchemy(app)
+
+# Initialize Bcrypt with the Flask app
 bcrypt = Bcrypt(app)
+
+# Set the secret key for the Flask app from environment variables
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.secret_key = os.getenv("SECRET_KEY") 
+
+# Initialize Marshmallow with the Flask app
 ma = Marshmallow(app)
 
