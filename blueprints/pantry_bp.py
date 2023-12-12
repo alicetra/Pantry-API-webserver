00a31e1338f3f75493ccb7dba4d1d1d976b30e3b
alicetra <alicetra0109@gmail.com>
2023-12-12 17:39:23 +1100

    put item and get item runout route completed

--- blueprints/pantry_bp.py
+++ blueprints/pantry_bp.py
@@ -39,7 +39,6 @@ def get_pantry():
     # This line checks if there are any items in the pantry.
     if pantry_items:
         # If there are items, it returns a 200 status code (indicating success) and a list of the items in dictionary form. 
-        # The `pantry_item_to_dict` function leverages the relationship between the `Pantry` and `PantryItem` models to convert each item into a dictionary.
         return create_response([pantry_item_to_dict(item) for item in pantry_items], 200)
     # If there are no items in the pantry.
     else:
@@ -117,10 +116,10 @@ def post_pantry_item():
     
     # This line checks if the count of the item is 0. If it is
     if data['count'] == 0:
-        # the run_out_time of the item is set to the current time.
+        # the run_out_time of the item is set to the current time. 
         new_item.run_out_time = datetime.now()
 
-    # This line adds the new item to the user's pantry. The relationship between the `Pantry` and `PantryItem` models allows us to easily add items to a pantry.
+    # This line adds the new item to the user's pantry. 
     pantry.items.append(new_item) 
     # This line commits the changes to the database. This saves the new item in the database.
     db.session.commit()
@@ -142,9 +141,10 @@ def delete_pantry():
 
     pantry = user.pantry 
 
-    # This line searches for the  item in the pantry's items. If it exists, it will be returned; otherwise, None will be returned.
+    # get the item in the pantry that matches the provided item that is normalised now, or None if no such item exists
+    # The relationship between the `Pantry` and `PantryItem` models allows us to directly access the items in a user's pantry using the `items` attribute.
     pantry_item = next((item for item in pantry.items if item.item == normalized_item), None)
-    # if item exist
+    # if item exist and a match has been found
     if pantry_item:
         # this line will delete it from the database.
         db.session.delete(pantry_item)
@@ -160,3 +160,106 @@ def delete_pantry():
     else:
         return create_response(f"{normalized_item} doesn't exist in the database", 400)
 
+@pantry_bp.route("/item", methods=["PUT"])
+# This route is a jwt required one since I only want the user to be allowed to ammend the data of their items.
+@jwt_required()
+def update_pantry():
+    schema = UpdatePantryItemSchema()
+    data = validate_data(request, schema)
+    if isinstance(data, tuple): 
+        return data
+
+    user = get_current_user()
+
+    normalized_item = normalize_item(data['item']) 
+
+    pantry = user.pantry  
+
+    pantry_item = next((item for item in pantry.items if item.item == normalized_item), None)
+
+    # This line sets the variable 'updated' to False as an initial state.
+    # This variable is used to track whether any actual update operation has been performed on the pantry item.
+    # If either 'count' or 'used_by_date' (or both) are not provided in the request data, 
+    # the pantry item will not be updated even though the item name (which is the only required field in the schema) is provided.
+    # In such a case, we don't want the API to incorrectly indicate that an update operation has been performed.
+    # Therefore, we initialize 'updated' to False and only set it to True when we actually update a field of the pantry item.
+    updated = False
+
+    # This line checks if pantry_item is None. If it is, it means that no item matching the normalized_item was found in the pantry.
+    if pantry_item is None:
+        # This line returns a response indicating that the item doesn't exist in the database, along with a 400 status code.
+        # The return statement immediately ends the function execution, and the rest of the code will not be executed.
+        return create_response(f"{normalized_item} doesn't exist in the database", 400)
+
+    # Note that I am doing the staticmethod validations after the code check that the item exist in the database first.
+    # If the item doesn't exist, there will be no update and therefore a validation is not required of the new data.
+    # This avoid unecessary computations.
+    fields = schema.fields.keys()
+    data_dict = prepare_data_dict(data, fields, PantryItem)
+
+    response = validate_fields(data_dict)
+    if response:
+        return response
+
+    # This line checks if 'used_by_date' is a key in the request (inputted) data.
+    if 'used_by_date' in data:
+         # this line calls the check_no_change function to see if the new used_by_date is different from the current one.
+         # If they are the same, check_no_change will return a response indicating that no update is needed, and this response will be immediately returned to the client and 
+         # the rest of the code will not be executed.
+        response = check_no_change(pantry_item.used_by_date, data['used_by_date'], f"{normalized_item} could not update used_by_date because the data is the same")
+        if response:
+            return response
+        # If the used_by_date in the request data is different from the current one, this line updates the pantry item's used_by_date with the new date.
+        pantry_item.used_by_date = data['used_by_date']
+        # Since an update has been made, this line sets 'updated' to True.
+        updated = True
+
+    # This line checks if 'count' is a key in the request (inputted) data.    
+    if 'count' in data:
+        # If 'count' is in the data, this line calls the check_no_change function to see if the new count is different from the current one.
+        # If they are the same, check_no_change will return a response indicating that no update is needed, and this response will be immediately returned to the client and 
+        # the rest of the code will not be executed.
+        response = check_no_change(pantry_item.count, data['count'], f"{normalized_item} could not update count because the data is the same")
+        if response:
+            return response
+        
+        #checks if the pantry was empty in the current databased and is now being restocked through the user input. 
+        if pantry_item.count == 0 and data['count'] > 0:
+            #If so then reset the pantry_item.run_out_time
+            pantry_item.run_out_time = None
+    
+        # This line updates the pantry item's count with the new count from the request data.
+        pantry_item.count = data['count']
+
+        # if the new inputted count is 0,meaning we have ran out of stock of this item
+        if pantry_item.count == 0:
+            # this line sets the pantry_item's run_out_time to the current time. 
+            pantry_item.run_out_time = datetime.now()
+        # Since an update has been made, this line sets 'updated' to True.
+        updated = True
+
+    # if update has been set to True at any point in the code 
+    if updated is True:
+        # Commit all the change(s)
+        db.session.commit()
+        return create_response(f"{normalized_item} has been updated", 200)
+    else:
+        return create_response("No update since no amendment has been provided for either count or used_by_date or both", 400)
+
+
+@pantry_bp.route("/itemrunout", methods=["GET"])
+# This route is a jwt required one since I only want the user to be allowed to grab the items in their pantry that have ran out of stock.
+@jwt_required()
+def get_runout_items():
+    user = get_current_user()
+    # This line uses a list comprehension to create a list of pantry items where the count is 0, indicating that they are out of stock.
+    runout_items = [item for item in user.pantry.items if item.count == 0]
+    # This line checks if the runout_items list is not empty, which means there are out of stock items.
+    if runout_items:
+            # If there are out of stock items, this line returns a response with
+            # a list of these items, each in their dictionary format, along with a 200 status code..
+        return create_response([pantry_item_to_dict(item) for item in runout_items], 200)
+        # If there are no out of stock items (i.e., the runout_items list is empty), 
+        # this line returns a response indicating that there are no out of stock items, along with a 200 status code.
+    else:
+        return create_response("You have no out of stock items", 200)
